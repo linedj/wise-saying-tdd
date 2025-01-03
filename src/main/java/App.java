2f14b769c31f6a78b3bd9ee20b7e815bc95cf193
linedj <eunjun998@gmail.com>
2025-01-03 14:40:03 +0900

    ãwise-saying-tdd 11강

---
+++ src/main/java/App.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class App {
+
+    private final Scanner sc;
+    private int lastId;
+
+    public App(Scanner sc) {
+        this.sc = sc;
+        this.lastId = 0;
+    }
+
+    public void run() {
+        System.out.println("== 명언 앱 ==");
+
+        while (true) {
+
+            System.out.println("명령 ) ");
+            String cmd = sc.nextLine(); //
+
+            if (cmd.equals("종료")) {
+
+                System.out.println("명언앱을 종료합니다.");
+                break;
+
+            } else if(cmd.equals("등록")) {
+
+                System.out.println("명언 : ");
+                System.out.println("작가 : ");
+                System.out.println("%d번 명언이 등록되었습니다.".formatted(++lastId));
+            }
+        }
+    }
+}
