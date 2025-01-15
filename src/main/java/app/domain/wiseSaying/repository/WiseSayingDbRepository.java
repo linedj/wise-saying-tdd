6d238e61221ca1e7e3b6214db83e10b25b665b11
linedj <eunjun998@gmail.com>
2025-01-15 12:38:12 +0900

    dbrepo t4 g

--- src/main/java/app/domain/wiseSaying/repository/WiseSayingDbRepository.java
+++ src/main/java/app/domain/wiseSaying/repository/WiseSayingDbRepository.java
@@ -1,17 +1,26 @@
 package app.domain.wiseSaying.repository;
 import app.domain.wiseSaying.WiseSaying;
+import app.global.AppConfig;
+import app.standard.Util;
 import app.standard.simpleDb.SimpleDb;
 import app.standard.simpleDb.Sql;
 
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 public class WiseSayingDbRepository {
+    private static final String DB_PATH = AppConfig.getDbPath() + "/wiseSaying";
+    private static final String BUILD_PATH = DB_PATH + "/build/data.json";
 
     private final SimpleDb simpleDb;
 
     public WiseSayingDbRepository() {
         this.simpleDb = new SimpleDb("localhost", "root", "lldj123414", "wiseSaying__test");
+    }
+
+    public static String getBuildPath() {
+        return BUILD_PATH;
     }
 
     public void createWiseSayingTable() {
@@ -74,5 +83,14 @@ public class WiseSayingDbRepository {
                 .append("SELECT *")
                 .append("FROM wise_saying")
                 .selectRows(WiseSaying.class);
+    }
+
+    public void build() {
+        List<Map<String, Object>> mapList = findAll().stream()
+                .map(WiseSaying::toMap)
+                .toList();
+
+        String jsonStr = Util.Json.listToJson(mapList);
+        Util.File.write(BUILD_PATH, jsonStr);
     }
 }
